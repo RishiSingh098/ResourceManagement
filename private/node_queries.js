d00c12f09bf5e47e4e767ac828626d8892d6e4e0
Max Rona <maxrona@Maxs-MacBook-Pro.local>
2017-09-07 16:03:49 -0400

    Fixed Date Issues

--- private/node_queries.js
+++ private/node_queries.js
@@ -3,6 +3,8 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/rm';
 
+var weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
 function get_all_info(start_date, end_date, skills, levels, callback) {
 	MongoClient.connect(url, function(err, db) {
 		var steps = [];
@@ -16,8 +18,8 @@ function get_all_info(start_date, end_date, skills, levels, callback) {
 		steps.push({"$unwind" : "$allocations"})
 
 		// Step 3: Filter by date range
-		var sd = start_date.getTime() - 2628000000 - 86400000;
-		var ed = end_date.getTime() - 2628000000;
+		var sd = start_date.getTime() - 86400000;
+		var ed = end_date.getTime();
 		steps.push({
 			"$match" : {"$and" : [{"allocations.date" : {"$gte" : sd}},
 								  {"allocations.date" : {"$lte" : ed}}]}
@@ -63,48 +65,70 @@ function get_all_info(start_date, end_date, skills, levels, callback) {
 	})
 }
 
-get_all_info(new Date(2017,08,21,0,0), new Date(2017,08,25,0,0), ["Java", "Back End Web"], [2,2], function(err, docs) {
+function get_all_skills(callback) {
+	MongoClient.connect(url, function(err, db) {
+		db.collection('skills').find({}, function(err, cursor) {
+			var skills = [];
+			cursor.each(function(err, doc) {
+				if(err != null) {
+					callback(err, null)
+				}
+				else if(doc != null) {
+					skills.push(doc.skill)
+				} else {
+					db.close()
+					callback(null, skills)
+				}
+			})
+		})
+	})
+}
+
+function lookup_employee(wwid, callback) {
+	MongoClient.connect(url, function(err, db) {
+		db.collection('employees').find({"wwid" : wwid}, function(err, cursor) {
+			if(cursor.count() == 0) {
+				callback("No Employee Found", null)
+			} else if(cursor.count()> 1) {
+				callback("ERROR: Duplicate WWID", null)
+			} else {
+				cursor.nextObject(function(err, doc) {
+					callback(null, doc);
+				})
+			}
+		})
+	})
+}
+
+function all_locations(callback) {
+	MongoClient.connect(url, function(err, db) {
+		db.collection('employees').distinct("location", function(err, locs) {
+			callback(null, locs);
+			db.close();
+		})
+	})
+}
+
+function all_teams(callback) {
+	MongoClient.connect(url, function(err, db) {
+		db.collection('employees').distinct("team", function(err, locs) {
+			callback(null, locs);
+			db.close();
+		})
+	})
+}
+
+// all_teams(function(err, locs) {
+// 	console.log(locs);
+// })
+
+get_all_info(new Date(2017,07,21,0,0), new Date(2017,07,25,0,0), ["Java", "Back End Web"], [2,2], function(err, docs) {
 	if(err != null) {
 		console.log(err);
 	} else {
 		console.log(docs)
 	}
 })
-
-
-// function find_java(db, callback) {
-// 	var collection = db.collection('employees');
-// 	collection.find({"skills.Java" : {"$exists" : true}}).toArray(function(err, docs) {
-// 		console.log(docs);
-// 		callback();
-// 	})
-// }
-
-// function get_all_skills(callback) {
-// 	MongoClient.connect(url, function(err, db) {
-// 		db.collection('skills').find({}, function(err, cursor) {
-// 			var skills = [];
-// 			cursor.each(function(err, doc) {
-// 				if(err != null) {
-// 					callback(err, null)
-// 				}
-// 				else if(doc != null) {
-// 					skills.push(doc.skill)
-// 				} else {
-// 					db.close()
-// 					callback(null, skills)
-// 				}
-// 			})
-// 		})
-// 	})
-// }
-
-// get_all_skills(function(err, skills) {
-// 	console.log(skills)
-// })
-
-// console.log("__________________")
-// console.log("all_skills")
 
 
 
